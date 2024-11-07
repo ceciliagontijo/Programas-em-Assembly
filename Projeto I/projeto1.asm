eab6ca6967d272bca072fd260a5f650d67f996ed
Cecilia Gontijo <ceciliagontijo50@gmail.com>
2024-11-07 10:38:27 -0300

    atualizacao do projeto

--- Projeto I/projeto1.asm
+++ Projeto I/projeto1.asm
@@ -9,7 +9,15 @@ pula_linha MACRO
     int 21h
     pop dx
     pop ax
-    
+ENDM
+resultado macro numero
+    push ax
+    push dx
+    mov ah, 9
+    lea dx, numero
+    int 21h
+    pop dx
+    pop ax
 ENDM
 .data
      matriz db  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
@@ -22,15 +30,15 @@ ENDM
             db  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
             db  0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0  
             db  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-            db  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0  
-            db  0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-            db  0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0  
-            db  0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            db  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 0  
+            db  0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            db  0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0  
+            db  0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
             db  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
             db  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-            db  4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-            db  4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-            db  4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            db  6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            db  6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            db  6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
             db  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
       tiros db  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
@@ -62,6 +70,11 @@ ENDM
     hidro db 10, 13, 'Voce acertou um hidroaviao!!!$'
     errou db 10, 13, 'Voce nao acertou nenhuma embarcacao!!!$'
     continua db 10, 13, 'Deseja atirar novamente? (s/n)$'
+    @um db 10, 13, 'Voce eliminou a embarcacao de encouracado.$'
+    @dois db 10, 13, 'Voce eliminou a embarcacao de fragata.$'
+    @tres db 10, 13, 'Voce eliminou uma embarcacao de submarino.$'
+    @quatro db 10, 13, 'Voce eliminou uma embarcacao de hidroaviao.$'
+    acerto db 6 dup (0)
 
 .code
 main proc 
@@ -110,44 +123,57 @@ sai2:
     mov si, bx                  
     pop ax
     pop bx
-    mov al, matriz[bx][si]          ;movendo para al para comparar o numero 
-    cmp al, 1      
-    je um
-    cmp al, 2
-    je dois
-    cmp al, 3
-    je tres
-    cmp al, 4
-    je quatro
-    mov ah, 9                       ;se nao move x 
-    lea dx, errou
-    int 21h
-    mov tiros[bx][si], 'X'
+    mov ch, matriz[bx][si]          ;movendo para al para comparar o numero 
+    cmp ch, 1 
+    je um1
+    cmp ch, 2
+    je dois2
+    cmp ch, 3
+    je tres3
+    cmp ch, 4
+    je quatro4
+    cmp ch, 5
+    je cinco5
+    cmp ch, 6
+    je seis6
+    call error
     jmp saida
-um:
-    mov tiros[bx][si], 1
-    mov ah, 9
-    lea dx, encou
-    int 21h
-jmp saida
-dois:
-    mov tiros[bx][si], 2
-     mov ah, 9
-    lea dx, frag
-    int 21h
-jmp saida
-tres:
-    mov tiros[bx][si], 3
-     mov ah, 9
-    lea dx, subma
-    int 21h
-jmp saida
-quatro:
-    mov tiros[bx][si], 4
-    mov ah, 9
-    lea dx, hidro
-    int 21h
-jmp saida
+um1:     
+    call um
+    cmp cl, 4
+    jne saida
+    resultado @um
+    jmp saida
+dois2:
+    call dois
+    cmp cl, 3
+    jne saida
+    resultado @dois
+    jmp saida
+tres3:
+    call tres
+    cmp cl, 2
+    jne saida
+    resultado @tres
+    jmp saida
+quatro4:
+    call quatro
+    cmp cl, 4
+    jne saida
+    resultado @tres
+    jmp saida
+cinco5:
+    call cinco
+    cmp cl, 4
+    jne saida
+    resultado @quatro
+    jmp saida
+seis6:
+    call seis
+    cmp cl, 4
+    jne saida
+    resultado @quatro
+  
 saida:
     pula_linha
     xor bx, bx
@@ -166,6 +192,7 @@ loop l2
     add bx, 20
     dec di
 jnz l1
+continuar:
     mov ah, 9
     lea dx, continua        ;verifica se quer continuar o programa 
     int 21h
@@ -197,7 +224,76 @@ jnz f1
     mov ah, 4ch
     int 21h
 main endp
+
+um proc
+    mov tiros[bx][si], 1
+    mov ah, 9
+    lea dx, encou
+    int 21h
+    xor di, di
+    inc acerto[di]
+    mov cl, acerto[di]
+    ret
+um endp
+dois proc 
+    mov tiros[bx][si], 2
+    mov ah, 9
+    lea dx, frag
+    int 21h
+    mov di, 1
+    inc acerto[di]
+    mov cl, acerto[di]
+    ret
+dois endp
+tres proc
+    mov tiros[bx][si], 3
+    mov ah, 9
+    lea dx, subma
+    int 21h
+    mov di, 2
+    inc acerto[di]
+    mov cl, acerto[di]
+    ret
+tres endp
+quatro proc 
+    mov tiros[bx][si], 4
+    mov ah, 9
+    lea dx, subma
+    int 21h
+    mov di, 3
+    inc acerto[di]
+    mov cl, acerto[di]
+    ret
+quatro endp
+cinco proc
+    mov tiros[bx][si], 5
+    mov ah, 9
+    lea dx, hidro
+    int 21h
+    mov di, 4
+    inc acerto[di]
+    mov cl, acerto[di]
+    ret
+cinco endp
+seis proc 
+    mov tiros[bx][si], 6
+    mov ah, 9
+    lea dx, hidro
+    int 21h
+    mov di, 5
+    inc acerto[di]
+    mov cl, acerto[di]
+    ret 
+seis endp
+error proc
+    mov ah, 9                       ;se nao move x 
+    lea dx, errou
+    int 21h
+    mov tiros[bx][si], 'X'
+    ret
+error endp
 end main
+
     
 
 
